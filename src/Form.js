85f0c6ed89c70edc9beebaa5a073a9a4dfd130ea
Sam Pritchard <sampritchard@msn.com>
2018-06-22 13:53:28 +0100

    basic client error checking on register

--- src/Form.js
+++ src/Form.js
@@ -6,47 +6,91 @@ export default class Form extends React.Component {
     state = {
         name: '',
         email: '',
-        password: ''
+        password: '',
+    }
+
+    checkForErrors = () => {
+        let isError = false;
+        const errors = {
+            nameError: '',
+            emailError: '',
+            passwordError: ''
+        };
+
+        if (this.state.name.length < 1) {
+            isError = true;
+            errors.nameError = true;
+        }
+
+        if (this.state.email.indexOf("@") === -1) {
+            isError = true;
+            errors.emailError = true;
+          }
+        
+        if (this.state.password.length < 6) {
+            isError = true;
+            errors.passwordError = true;
+        }
+
+            this.setState({
+                ...this.state,
+                ...errors
+            })
+
+        return isError;
     }
 
     onSubmit = e => {
         e.preventDefault();
-        console.log(this.state);
-        this.setState({
-            name: '',
-            email: '',
-            password: ''
-        })
+        console.log(this.state)
+
+        const err = this.checkForErrors();
+        if (!err) {
+            this.setState({
+                name: '',
+                nameError: '', 
+                email: '',
+                emailError: '',
+                password: '',
+                passwordError: ''
+            })
+        }
     }
 
     render() {
         return (
             <form>
                 <TextField
+                    autoFocus
                     label="Name"
-                    floatingLabelText="Name"
                     value={this.state.name} 
-                    onChange={e => this.setState({ name: e.target.value })} 
-                    floatingLabelFixed
+                    onChange={e => this.setState({ name: e.target.value })}
+                    error={this.state.nameError}
+                    errorText={this.state.nameError}
                 />
                 <br />        
                 <TextField
                     label="Email"
-                    floatingLabelText="Email"
                     value={this.state.email} 
                     onChange={e => this.setState({ email: e.target.value })} 
-                    floatingLabelFixed
+                    error={this.state.emailError}
+                    errorText={this.state.emailError}
                 />
                 <br />                
                 <TextField
                     label="Password"
-                    floatingLabelText="Password"
+                    type="Password"
                     value={this.state.password} 
-                    onChange={e => this.setState({ password: e.target.value })} 
-                    floatingLabelFixed
+                    onChange={e => this.setState({ password: e.target.value })}
+                    error={this.state.passwordError}
+                    errorText={this.state.passwordError}
+                    helperText="Minimum 5 characters"
                 />
-                <br />                
-                <Button onClick={ e => this.onSubmit(e) }>Submit</Button>
+                <br />   
+                <br />             
+                <Button onClick={ e => this.onSubmit(e) }variant="outlined" color="primary">
+        Submit
+      </Button>
             </form>
         )
     }
